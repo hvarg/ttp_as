ea94086fd2b8c0ead5ff8953ac268a32a09152e9
Hernán Vargas <hernan133@gmail.com>
2016-07-01 21:48:07 -0400

    src: Agregada estructura de resultados y funciones free.

--- src/structures.c
+++ src/structures.c
@@ -1,73 +1,78 @@
 #include "structures.h"
 
-/* Create a new list. */
-struct list *new_list()
-{
-  struct list * new = (struct list *) malloc(sizeof(struct list));
-  new->first = NULL;
-  new->last  = NULL;
-  new->size  = 0;
-  return new;
-}
-
-/* Add a element to a list. The element is stored in a 'item'. */
-void list_add(struct list *alist, void *element)
-{
-  struct item *new = (struct item *) malloc(sizeof(struct item));
-  new->value = element;
-  new->next  = NULL;
-  if (alist->first == NULL) {
-    alist->first = new;
-    alist->last  = new;
-  } else {
-    (*alist->last).next = new;
-    alist->last = new;
-  }
-  alist->size++;
-}
-
-/* Extrae el primer elemento de la lista. Ojo con el free */
-void *extract_first(struct list *alist)
-{
-  struct item *tmp = alist->first;
-  void *val = tmp->value;
-  if (alist->last == tmp)
-    alist->last = NULL;
-  alist->first = tmp->next;
-  alist->size--;
-  free(tmp);
-  return val;
-}
-
-/* Delete a list (and free). */
-void list_del(struct list *alist)
-{
-  struct item *act, *next;
-  for(act = alist->first; act != NULL; act = next){
-    next = act->next;
-    free(act->value);
-    free(act);
-  }
-  free(alist);
-}
-
-/******************************************************************************/
-struct instance *new_instance(char *name)
+struct instance *new_instance (char *name)
 {
   struct instance *ins = (struct instance *) malloc(sizeof(struct instance));
   ins->name      = name;
   ins->s_teacher = 0;
   ins->s_class   = 0;
   ins->s_room    = 0;
+  ins->times     = NULL;
+  ins->teachers  = NULL;
+  ins->rooms     = NULL;
+  ins->classes   = NULL;
+  ins->events    = NULL;
   return ins;
 }
 
-struct event *new_event(short duration)
+struct event *new_event (short duration)
 {
   struct event *ev = (struct event *) malloc(sizeof(struct event));
   ev->duration = duration;
-  ev->class = -1; ev->room = -1; ev->teacher = -1;
+  ev->class    = -1;
+  ev->room     = -1;
+  ev->teacher  = -1;
   return ev;
+}
+
+struct result *new_result (struct instance *ins)
+{
+  int i;
+  struct result *r = (struct result *) malloc(sizeof(struct result));
+  r->ins  = ins;
+  r->txt  = (bool **)  malloc(ins->s_teacher * sizeof(bool *) );
+  r->cxt  = (bool **)  malloc(ins->s_class   * sizeof(bool *) );
+  r->rxt  = (bool **)  malloc(ins->s_room    * sizeof(bool *) );
+  r->ph   = (float **) malloc(ins->s_event   * sizeof(float *));
+  r->path = (short *)  malloc(ins->s_event   * sizeof(short)  );
+  for(i=0; i < ins->s_teacher; r->txt[i++] = malloc(ins->s_time*sizeof(bool)));
+  for(i=0; i < ins->s_class;   r->cxt[i++] = malloc(ins->s_time*sizeof(bool)));
+  for(i=0; i < ins->s_room;    r->rxt[i++] = malloc(ins->s_time*sizeof(bool)));
+  for(i=0; i < ins->s_event;   r->ph[i++] = malloc(ins->s_event*sizeof(float)));
+  return r;
+}
+
+void del_instance (struct instance *ins)
+{
+  int i;
+  if (ins->times    != NULL) free(ins->times   );
+  if (ins->teachers != NULL) free(ins->teachers);
+  if (ins->rooms    != NULL) free(ins->rooms   );
+  if (ins->classes  != NULL) free(ins->classes );
+  if (ins->events   != NULL) 
+    for (i = 0; i < ins->s_event; del_event(ins->events[i++]));
+  free(ins);
+}
+
+void del_event (struct event *ev)
+{
+  free(ev);
+}
+
+/* Siempre usar antes de del_instance. */
+void del_result (struct result *r)
+{
+  int i;
+  for(i=0; i < r->ins->s_teacher; free(r->txt[i++]));
+  for(i=0; i < r->ins->s_class;   free(r->cxt[i++]));
+  for(i=0; i < r->ins->s_room;    free(r->rxt[i++]));
+  for(i=0; i < r->ins->s_event;   free(r->ph[i++]) );
+  free(r->txt ); 
+  free(r->cxt ); 
+  free(r->rxt ); 
+  free(r->ph  ); 
+  free(r->path);
+  free(r);
 }
 
 /* vim: set ts=2 sw=2 sts=2 tw=80 : */
--- src/structures.h
+++ src/structures.h
@@ -1,36 +1,44 @@
 #ifndef STRUCTURES_H_
 #define STRUCTURES_H_
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct item{
-  void *value;
-  struct item *next;
-};
-
-struct list{
-  struct item *first;
-  struct item *last;
-  unsigned int size;
-};
-
-struct list *new_list();
-void list_add(struct list *alist, void *element);
-void *extract_first(struct list *alist);
-void list_del(struct list *alist);
-
-/***********************************/
 struct instance{
+  /* Equivalentes entre id (int) y su tag (str). */
   char  *name, **times, **teachers, **rooms, **classes;
+  /* Numero de recursos. */
   short s_time, s_teacher, s_room, s_class, s_event;
+  /* Arreglo de eventos, su indice es su id. */
   struct event **events;
 };
 
 struct event{
-  short duration, class, teacher, room;
+  /* Duracion del evento.  */
+  short duration;
+  /* Id de los recursos requeridos (-1 cualquiera).*/
+  short class, teacher, room;
 };
 
-struct instance *new_instance (char *name);
-struct event    *new_event    (short duration);
+struct result{
+  /* Puntero a la instancia resuelta. */
+  struct instance *ins;
+  /* Tablas de utilización de recursos por tiempo:
+   *  txt: Teacher X Time
+   *  cxt: Class X Time
+   *  rxt: Room X Time */
+  bool   **txt, **cxt, **rxt;
+  /* Tabla de feromonas. ph[Evento_inicial][Evento_final]*/
+  float  **ph;
+  /* Camino de eventos. */
+  short  *path;
+};
+
+struct instance   *new_instance   (char *name);
+struct event      *new_event      (short duration);
+struct result     *new_result     (struct instance *ins);
+void              del_instance    (struct instance *ins);
+void              del_event       (struct event *ev);
+void              del_result      (struct result *r);
 
 #endif
 /* vim: set ts=2 sw=2 sts=2 tw=80 : */
